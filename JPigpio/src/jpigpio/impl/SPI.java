f57087fe262ea52a605f83b9629214fd7db23468
Christian Fischer <sw-dev@computerlyrik.de>
2017-02-26 21:13:37 +0100

    set ce for spi if present

--- JPigpio/src/jpigpio/impl/SPI.java
+++ JPigpio/src/jpigpio/impl/SPI.java
@@ -1,13 +1,20 @@
 package jpigpio.impl;
 
+import jpigpio.GPIO;
 import jpigpio.JPigpio;
 import jpigpio.PigpioException;
 import jpigpio.Utils;
 
 public class SPI {
 	private int handle;
-	private JPigpio pigpio;
+	private final JPigpio pigpio;
 	private boolean debug;
+	private GPIO ce;
+
+    public SPI(GPIO ce, int channel, int baudRate, int flags) throws PigpioException {
+        this(ce.getPigpio(), channel, baudRate, flags);
+        this.ce = ce;
+    }
 
 	public SPI(JPigpio pigpio, int channel, int baudRate, int flags) throws PigpioException {
 		this.pigpio = pigpio;
@@ -20,7 +27,9 @@ public class SPI {
 	} // End of close
 
 	public void read(byte data[]) throws PigpioException {
+        ceSetValue(JPigpio.PI_LOW);
 		pigpio.spiRead(handle, data);
+        ceSetValue(JPigpio.PI_HIGH);
 		if (debug) {
 			System.out.println("spiRead: " + Utils.dumpData(data));
 		}
@@ -30,25 +39,34 @@ public class SPI {
 		if (debug) {
 			System.out.println("spiWrite: " + Utils.dumpData(data));
 		}
+        ceSetValue(JPigpio.PI_LOW);
 		pigpio.spiWrite(handle, data);
-
+        ceSetValue(JPigpio.PI_HIGH);
 	} // End of write
 
 	public void xfer(byte txData[], byte rxData[]) throws PigpioException {
 		if (debug) {
 			System.out.print("xfer: " + Utils.dumpData(txData));
 		}
+        ceSetValue(JPigpio.PI_LOW);
 		pigpio.spiXfer(handle, txData, rxData);
+        ceSetValue(JPigpio.PI_HIGH);
 		if (debug) {
 			System.out.println(" " + Utils.dumpData(rxData));
 		}
 	} // End of xfer
-	
+
 	public byte xfer(byte txData) throws PigpioException {
 		byte txData1[] = {txData};
 		byte rxData[] = new byte[1];
 		xfer(txData1, rxData);
 		return rxData[0];
 	}
+
+	    private void ceSetValue(boolean value) throws PigpioException {
+	        if (ce != null) {
+	            ce.setValue(value);
+	        }
+	    }
 } // End of class
 // End of file
