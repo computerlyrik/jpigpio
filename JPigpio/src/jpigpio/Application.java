0bdd02219f7091222a2bf5d68cbf9d7c0d7264bb
Christian Fischer <sw-dev@computerlyrik.de>
2017-02-27 13:37:36 +0100

    update in SPI test

--- JPigpio/src/jpigpio/Application.java
+++ JPigpio/src/jpigpio/Application.java
@@ -13,20 +13,22 @@ public class Application {
         int chipSelectPin = JPigpio.PI_SPI_CE0;
         int soPin = JPigpio.PI_SPI_MISO;
 
-        if (pigpio.gpioGetMode(chipSelectPin) != JPigpio.PI_OUTPUT) {
-            throw new WrongModeException(chipSelectPin);
-        }
-        if (pigpio.gpioGetMode(soPin) != JPigpio.PI_INPUT) {
-            throw new WrongModeException(soPin);
-        }
+        pigpio.gpioSetMode(chipSelectPin, JPigpio.PI_OUTPUT);
+        pigpio.gpioSetMode(soPin, JPigpio.PI_INPUT);
+
         pigpio.gpioWrite(chipSelectPin, JPigpio.PI_HIGH);
         pigpio.gpioWrite(chipSelectPin, JPigpio.PI_LOW);
         while (pigpio.gpioRead(soPin) != JPigpio.PI_LOW) {
             System.out.println("Waiting for so to go low");
             pigpio.gpioDelay(500);
         }
+
         System.out.print("Reset complete");
         SPI spi = new SPI(pigpio, 0, JPigpio.PI_SPI_BAUD_8MHZ, 0);
+
+        System.out.print("After SPI Setup CE: "+pigpio.gpioGetMode(chipSelectPin));
+        System.out.print("After SPI Setup SO: "+pigpio.gpioGetMode(soPin));
+
         byte[] txData = new byte[]{0x30};
         byte[] rxData = new byte[2];
         spi.xfer(txData, rxData);
@@ -34,6 +36,10 @@ public class Application {
         for (byte b : rxData) {
             System.out.println(Integer.toHexString(b));
         }
+
+
+        {
+            if (pigpio.gpioGetMode(soPin) != JPigpio.PI_INPUT) {
 
         pigpio.gpioTerminate();
 
